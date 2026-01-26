75a6e9da9b2366b47159eb5a2c1a5e08b0057264
chuan <15512109007@163.com>
2026-01-26 14:37:52 +0800

    舵机转动

--- linker51-mcu/main.c
+++ linker51-mcu/main.c
@@ -6,8 +6,15 @@ typedef unsigned char u8;
 sbit Servo_Pin = P1^1;
 
 u8 count = 0;
-u8 target = 5;
-unsigned int i, j;
+u8 target_pos = 5;
+
+void uart_init(u8 baud) {
+    SCON = 0x50;
+    TMOD |= 0x20;
+    PCON=0x80;
+    TH1 = baud; TL1 = baud;
+    TR1 = 1; ES = 1; EA = 1;
+}
 
 void timer0_init(){
 	TMOD &=0xF0;
@@ -16,23 +23,11 @@ void timer0_init(){
     ET0 = 1; EA = 1; TR0 = 1;
 }
 
-void delay_ms(unsigned int ms) {
-    for(i=0; i<ms; i++)
-        for(j=0; j<120; j++);
-}
-
 void main() {
+	uart_init(0XFA);
     timer0_init();
 
-    while(1) {
-        target = 5;  // 0
-        delay_ms(1000);
-
-        target = 13; // 90
-        delay_ms(1000);
-
-        target = 22; // 180
-        delay_ms(1000);		
+    while(1) {		
     }
 }
 
@@ -40,12 +35,27 @@ void timer0_isr() interrupt 1 {
     TH0 = 0xFF;
     TL0 = 0xA4;
 
+    count++;
     if(count >= 200) {
         count = 0;
     }
-    if(count < target){
+    if(count < target_pos){
         Servo_Pin = 1;
     }else {
         Servo_Pin = 0;
     }
+}
+
+void uart() interrupt 4{
+	if(RI){
+		RI = 0;
+		target_pos = SBUF;
+
+        if (target_pos < 5) target_pos = 5;
+        if (target_pos > 22) target_pos = 22;
+
+		SBUF = target_pos;
+		while(!TI);
+		TI=0;
+	}
 }
--- linker51-python/main.py
+++ linker51-python/main.py
@@ -2,15 +2,15 @@ import config
 import cv2
 from core.communicator import Communicator
 from core.vision_simulator import ImageProcessor
-from core.motor_controller import MotorController
+from core.servo_ontroller import ServoController
 
 def main():
     # 初始化串口通信
     communicator = Communicator(config.SERIAL_PORT, config.BAUDRATE)
-    # 初始化电机逻辑控制器
-    motor = MotorController(communicator)
+    # 初始化专门的舵机控制器
+    servo = ServoController(communicator)
     # 指定测试图片
-    img_path = "assets/mid.png"
+    img_path = "assets/left.png"
     processor = ImageProcessor(img_path)
     print(f"--- 正在分析测试图片: {img_path} ---")
     try:
@@ -20,14 +20,14 @@ def main():
         cv2.circle(frame, (x, y), 20, (0, 255, 0), 2)
         cv2.imshow("Debug View", frame)
         if x is not None:
-            success = motor.update_by_x(x)
+            success = servo.track_x(x)
             if success:
                 print(" -> 发送成功。")
             else:
                 print(" -> 发送失败。")
         cv2.waitKey(0)
 
-        communicator.send_level(0)
+        communicator.send_level(5)
     except KeyboardInterrupt:
         print("\n Ctr+C 手动停止。")
     finally:
